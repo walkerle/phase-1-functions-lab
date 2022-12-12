f20185ea19d08c640b1442b4315d0d469b234a35
Walker Le <walker.le88@gmail.com>
2022-12-12 16:58:06 -0700

    commit tree 1b333e47e787212ce0ff6c7960b60d2abf859669

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupBlock) {
+    // if (pickupBlock > 42) {
+    //     return pickupBlock - 42;
+    // } else {
+    //     return 42 > pickupBlock;
+    // }
+    return Math.abs(pickupBlock - 42);
+}
+
+function distanceFromHqInFeet(pickupBlock) {
+    return distanceFromHqInBlocks(pickupBlock) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const totalDistance = distanceTravelledInFeet(start, destination);
+    // if (totalDistance <= 400) {
+    //     return 0;
+    // } else if (totalDistance > 400 && totalDistance <= 2000) {
+    //     return (totalDistance - 400) * 0.02;
+    // } else if (totalDistance > 2000 && totalDistance <= 2500) {
+    //     return 25;
+    // } else {
+    //     return 'cannot travel that far';
+    // }
+
+    switch (true) {
+        case totalDistance <= 400:
+            return 0;
+        case (totalDistance > 400 && totalDistance <=2000):
+            return (totalDistance - 400) * 0.02;
+        case (totalDistance > 2000 && totalDistance <= 2500):
+            return 25;
+        default:
+            return 'cannot travel that far';
+    }
+}
